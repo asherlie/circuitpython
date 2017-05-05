8ed844e3935ba32415784640edb53451a95386ec
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-05-04 17:04:50 -0700

    more work

--- atmel-samd/samd21_pins.h
+++ atmel-samd/samd21_pins.h
@@ -32,6 +32,8 @@
 #define __MICROPY_INCLUDED_ATMEL_SAMD_BOARDS_SAMD21_PINS_H__
 >>>>>>> WIP
 
+#include "include/sam.h"
+
 #include "common-hal/microcontroller/Pin.h"
 
 void reset_pin(uint8_t pin);
